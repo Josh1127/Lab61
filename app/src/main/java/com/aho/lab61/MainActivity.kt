40c6af59677c45bee6619d43000f55ce19635fc2
Josh1127 <josh.chiu1127@gmail.com>
2020-05-26 15:29:38 +0800

    initial commit

---
+++ app/src/main/java/com/aho/lab61/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.aho.lab61
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val fruits = arrayOf("Apple","Banana","Cherry","Grapes","Lemon","Orange","Pear","Pineapple","Tomato","Watermelon")
+    }
+}
