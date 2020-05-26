9d9263544c18d623bc63c95967dfd513d0f13519
mu1127 <josh.chiu1127@gmail.com>
2020-05-26 22:59:11 +0800

    MyAdapter

---
+++ app/src/main/java/com/aho/lab61/Item.kt
@@ -0,0 +1,3 @@
+package com.aho.lab61
+
+class Item (var namw:String, var price: Int, var img: Int)
---
+++ app/src/main/java/com/aho/lab61/MyAdapter.kt
@@ -0,0 +1,21 @@
+package com.aho.lab61
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class MyAdapter : BaseAdapter(){
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+    }
+
+    override fun getItem(position: Int): Any {
+    }
+
+    override fun getItemId(position: Int): Long {
+    }
+
+    override fun getCount(): Int {
+    }
+
+}
